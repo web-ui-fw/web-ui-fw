4753cb3501259837c2140d5c80187250fb9b7b62
Salvatore Iovene <salvatore.iovene@intel.com>
2011-08-15 13:41:22 +0300

    Use short English month names as default.

--- widgets/jquery.mobile.datetimepicker/js/jquery.mobile.datetimepicker.js
+++ widgets/jquery.mobile.datetimepicker/js/jquery.mobile.datetimepicker.js
@@ -13,9 +13,9 @@
             showTime: true,
             header: "",
             timeSeparator: ":",
-            months: ["January", "February", "March", "April", "May",
-                     "June", "July", "August", "September", "October",
-                     "November", "December"],
+            months: ["Jan", "Feb", "Mar", "Apr", "May",
+                     "Jun", "Jul", "Aug", "Sep", "Oct",
+                     "Nov", "Dec"],
             animationDuration: 500,
             yearsRange: 30 
         },
