246456de1715cc5a92639d7443061ed91ca64aff
Rijubrata Bhaumik <rijubrata.bhaumik@intel.com>
2011-08-25 19:00:40 +0300

    should change .animate in next iteration

--- src/widgets/progressbar/js/progressbar.js
+++ src/widgets/progressbar/js/progressbar.js
@@ -14,66 +14,42 @@
 			duration	: 20000,
 		},
 				
-		min : 0,
-		
 		data: {
 			uuid		: 0,
 			bar         : 0,
 			box			: 0
 		},		
 		
+		_value: function() {
+			return  (this.options.value);
+		},
+		
+		_refreshValue: function(now) {
+			this.options.value = parseInt(now);
+			console.log("value = " + parseInt(this._value()));
+		},
+		
+		
 		startProgress: function(value) {
+			var obj = this;
 			this.data.bar.animate({
 				    width: '100%'
-				   }, this.options.duration, function() {
-				       // Animation complete Callback 
-				       _completed();
-			  	  },
-			  	  /*
-			  	  step: function(now,fx) {
-			  	  	var data = fx.elem.id + ' ' + fx.prop + ': ' + now;
-			  	  }
-			  	  */
+				  	}, {
+					    duration :this.options.duration,
+					    specialEasing : {
+					    width: 'linear',
+					   }, 
+						complete: function() {
+					   		//alert("animation completed : value is: " + parseInt(obj._value()));
+					   		// callback function here		
+					  },
+					  step : function( now, fx ) {
+					  	  obj._refreshValue(now);
+					  },
+			  	   }
 			  	 );
-		},
+		},		
 		
-		_completed: function() {
-			// animation is finished 
-			alert("callback func animation complete");	
-		}
-		
-		/*
-		_value: function() {
-			var val = this.options.value;
-			// normalize the invalid value
-			if (typeof val !== "number") {
-				val = 0;
-			}	
-			return Math.min(this.options.max, Math.max(this.min, val ));
-		},
-		*/
-		_destroy: function() {
-			/*
-			this.data.bar.remove();
-			this.data.box.remove();
-			this.text1.remove();
-			this.text2.remove();
-			this.text3.remove();
-			this.progressbar.remove();
-			this.cancel-button.remove();
-			this.element.removeClass("ui-upper-progressbar-container ui-progressbar"); 
-			this.cancelContainer.remove();
-			*/
-			console.log("_destroy is called");
-		},
-		
-		/*
-		_setOptions: function( key, value) {
-			if ( key === "value") {
-				this.options.value = value;
-			}
-		},
-		*/
 		_create: function() {
 			var container = this.element;
 			var obj = this;
@@ -83,7 +59,9 @@
             var myUUID = this.data.uuid;
             
             container.attr("id", "ui-progressbar-container" + this.data.uuid);
-  		
+            
+            console.log("creating ui-progressbar-container with id " + this.data.uuid);  
+			
 			var upperProgressBarContainer = $.createUpperProgressBarContainer();
 			upperProgressBarContainer.attr("id", "ui-upper-progressbar-container");
 			
@@ -93,7 +71,8 @@
 			}).appendTo(upperProgressBarContainer);
 			
 			$('<div/>', {
-				id: 'progressbar',														
+				id: 'progressbar',
+														
 			}).appendTo(upperProgressBarContainer);			
 			
 				
@@ -119,8 +98,10 @@
 				'data-inline': true,			
 			}).appendTo(cancelContainer);
 			
+			
 			container.append(upperProgressBarContainer);
 			container.append(cancelContainer);	
+			
 			
 			this.data.box = $('<div/>', {
 					id: 'boxImgId',					
@@ -128,7 +109,9 @@
 							
 			this.data.bar = $('<div/>', {
 					id: 'barImgId',					
-				}).appendTo(progressbar);						
+				}).appendTo(progressbar);			
+							
+			
 							
 			cancelContainer.find('#cancel-button').click(function() {
 				//alert("cancel button clicked");
@@ -136,7 +119,7 @@
 					obj.data.bar.stop();
 				};
 				obj.options.value =  parseInt(parseInt(obj.data.bar.css('width')) / parseInt(obj.data.box.css('width')) * 100 );
-				alert("value=" + obj.options.data.value);
+				alert("value=" + obj.options.value);
 			}); // end of click		
 			
 			/* caller of the progress bar widget should start the progressbar using startProgress */
