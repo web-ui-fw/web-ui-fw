7a676e55863e23d0cf4abfa1bb20e15afc5ed11a
Elliot Smith <elliot.smith@intel.com>
2011-09-05 18:05:35 +0100

    [spinner] Clean up API and add demo

--- src/widgets/spinner/spinner.js
+++ src/widgets/spinner/spinner.js
@@ -16,6 +16,9 @@
  * Apply a spinner using the spinner() method or by adding a
  * data-processing="spinner" attribute to an element.
  *
+ * Once you have a spinner, call the start() method on it to display it
+ * and start the animation.
+ *
  * The spinner uses a div directly after the element. Calling done()
  * on a spinner detaches this element from the DOM; it also removes
  * the data-processing="spinner" attribute from the target element, if one
@@ -23,19 +26,24 @@
  *
  * Options:
  *
- *   position = object; positioning specifier, allowing positioning of the
- *              spinner with respect to the target element; default
- *              is to align 'right center' of the spinner
- *              with 'right center' of the target element; change by passing
- *              an object with 'my' and 'at' properties, as per the jQuery UI
- *              position() options (see http://jqueryui.com/demos/position/);
- *              e.g. to position the center of the spinner over the center
- *              of the target element, use:
- *                  position: {my:'center center', at: 'center center'}
- *              where 'my' specifies the point on the spinner, and 'at'
- *              specifies the point on the target element
- *   duration = integer; number of seconds the spinner should take to rotate
-                the full 360 degrees
+ *     position: Object; positioning specifier, allowing positioning of the
+ *               spinner with respect to the target element; default
+ *               is to align 'right center' of the spinner
+ *               with 'right center' of the target element; change by passing
+ *               an object with 'my' and 'at' properties, as per the jQuery UI
+ *               position() options (see http://jqueryui.com/demos/position/);
+ *               e.g. to position the center of the spinner over the center
+ *               of the target element, use:
+ *                   position: {my:'center center', at: 'center center'}
+ *               where 'my' specifies the point on the spinner, and 'at'
+ *               specifies the point on the target element
+ *     duration: Integer; number of seconds the spinner should take to rotate
+ *               the full 360 degrees
+ *
+ * Events:
+ *
+ *     stopped: Fired when stop() is called on the spinner and it has been
+ *              detached from the DOM
  */
 
 (function($, undefined) {
@@ -43,42 +51,49 @@
 $.widget("TODONS.spinner", $.mobile.widget, {
     options: {
         initSelector: ":jqmData(processing='spinner')",
-        positioning: {my: 'right center', at: 'right center'},
-        duration: 1
+        position: {my: 'right center', at: 'right center'},
+        duration: 1,
+        isRunning: false
     },
 
     _create: function() {
-        var self = this,
-            target = this.element,
-            o = this.options,
-            popup, zIndex, spinner;
+        var popup, zIndex;
 
         popup = $.mobile.loadPrototype("spinner").find("div:first");
 
-        zIndex = target.css('z-index');
+        zIndex = this.element.css('z-index');
         zIndex = zIndex ? zIndex + 1 : 10;
         popup.css('z-index', zIndex);
 
-        target.after(popup);
-
-        o.positioning['of'] = target;
-
-        popup.position(o.positioning);
-
-        spinner = popup.find('.ui-spinner');
-
-        spinner.css('-webkit-animation-duration', o.duration + 's');
+        this.options.position['of'] = this.element;
 
         this.popup = popup;
     },
 
-    done: function () {
+    start: function () {
+        if (!this.isRunning) {
+            var spinner = this.popup.find('.ui-spinner');
+            spinner.css('-webkit-animation-duration', this.options.duration + 's');
+
+            this.element.after(this.popup);
+            this.popup.position(this.options.position);
+
+            this.isRunning = true;
+        }
+    },
+
+    stop: function () {
+      if (this.isRunning) {
+          this.popup.detach();
+          this.element.trigger('stopped');
+          this.isRunning = false;
+      }
     }
 });
 
 // auto self-init widgets; NB pageshow is used here so the DOM elements
 // have already been drawn before positioning is done
-$(document).bind("pageshow", function (e) {
+$(document).bind("pagecreate", function (e) {
     $($.TODONS.spinner.prototype.options.initSelector, e.target)
     .not(":jqmData(role='none'), :jqmData(role='nojs')")
     .spinner();
