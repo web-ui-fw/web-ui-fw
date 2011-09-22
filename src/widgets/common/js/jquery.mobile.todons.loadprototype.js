829ca15b5d9ed95242a5ee67facdc17ca924e82d
Gabriel Schulhof <gabriel.schulhof@intel.com>
2011-09-22 14:08:05 +0300

    loadPrototype optionally fills in and returns a hash of UI elements

--- src/widgets/common/js/jquery.mobile.todons.loadprototype.js
+++ src/widgets/common/js/jquery.mobile.todons.loadprototype.js
@@ -6,8 +6,54 @@ jQuery.extend( jQuery.mobile.todons,
 {
   _widgetPrototypes: {},
 
-  loadPrototype: function(widgetname) {
-    var ret = $.mobile.todons._widgetPrototypes[widgetname];
+  /*
+   * load the prototype for a widget.
+   *
+   * Looks for @widgetname.prototype.html in the proto-html/ subdirectory of the framework's current theme.
+   * Loads the file if found, and, if @ui is provided, fills in the hash by looking for the elements specified
+   * by the selectors in the hash.
+   * If the hash is provided, it will remove the "id" attribute for those elements that are selected by id.
+   * Examples:
+   *
+   * 1.
+   * $.mobile.todons.loadPrototype("mywidget") => Returns a <div> containing the structure from the file
+   * mywidget.prototype.html located in the current theme folder of the current framework.
+   *
+   * 2. $.mobile.todons.loadPrototype("mywidget", ui):
+   * ui is a hash that looks like this:
+   * ui = {
+   *   element1: "<css selector 1>",
+   *   element2: "<css selector 2>",
+   *   group1: {
+   *     group1element1: "<css selector 3>",
+   *     group1element1: "<css selector 4>"
+   *   }
+   *  ...
+   * }
+   *
+   * In this case, after loading the prototype as in Example 1, loadPrototype will traverse @ui and replace the CSS
+   * selector strings with the result of the search for the selector string upon the prototype. If any of the CSS
+   * selectors are of the form "#elementid" then the "id" attribute will be stripped from the elements selected. This
+   * means that they will no longer be accessible via the selector used initially. @ui is then returned thus modified.
+   */
+  loadPrototype: function(widgetname, ui) {
+    var selector,
+        ret = $.mobile.todons._widgetPrototypes[widgetname];
+
+    function fillObj(obj, uiProto) {
+      for (var key in obj) {
+        if (typeof obj[key] === "string") {
+          selector = obj[key];
+          obj[key] = uiProto.find(obj[key]);
+          if (selector.substring(0, 1) === "#")
+            obj[key].removeAttr("id");
+        }
+        else
+        if (typeof obj[key] === "object")
+          obj[key] = fillObj(obj[key], uiProto);
+      }
+      return obj;
+    }
 
     if (ret === undefined) {
       var theScriptTag = $("script[data-framework-version][data-framework-root][data-framework-theme]"),
@@ -27,8 +73,11 @@ jQuery.extend( jQuery.mobile.todons,
         });
     }
 
-    if (ret != undefined)
+    if (ret != undefined) {
       ret = ret.clone();
+      if (ui != undefined)
+        ret = fillObj(ui, ret);
+    }
 
     return ret;
   }
