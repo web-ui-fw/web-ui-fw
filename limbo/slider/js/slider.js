3603089f5c8eb36667fe53fcab04ecc6fffbd330
Elliot Smith <elliot.smith@intel.com>
2011-10-11 17:14:28 +0100

    [slider] Display slider value on handle

--- limbo/slider/js/slider.js
+++ limbo/slider/js/slider.js
@@ -3,99 +3,40 @@
  *
  * Copyright (C) TODO
  * License: TODO
- * Authors: Rijubrata Bhaumik <rijubrata.bhaumik@intel.com>
- */
-
-/**
- * Displays a slider element
- *
- * Options:
- *
+ * Authors:
  */
 
 (function ($, window, undefined) {
     $.widget("todons.todonsslider", $.mobile.widget, {
-        options: {
-        },
-
-        value: function( newValue ) {
-            if ( newValue === undefined ) {
-                return $(this.element).val();
-            } else {
-                $(this.element).val( newValue );
-            }
-        },
-
         _create: function() {
+            var self = this;
             var inputElement = $(this.element);
 
             // apply jqm slider
             inputElement.slider();
 
-            var container = inputElement.parent();
-            var slider = inputElement.siblings(".ui-slider");
-            var sliderHandle = slider.find(".ui-slider-handle");
-            var text = sliderHandle.find(".ui-btn-text");
-
-            container.addClass("ui-slider-container");
-
-            // remove corner radii
-            slider.removeClass("ui-btn-corner-all");
-            sliderHandle.removeClass("ui-btn-corner-all");
-
-            // add popup
-            var popupText=$("<div class='ui-slider-popup-text'></div>");
-            var popup=$("<div class='ui-slider-popup ui-shadow ui-slider-handle' data-theme='c'></div>");
-            popup.append(popupText);
-            sliderHandle.append(popup);
-
-            popup.position({my: 'center top',
-                            at: 'center bottom',
-                            offset: '0 0',
-                            of: container.find(".ui-slider-handle")});
-            popup.hide();
-
-            sliderHandle.bind( {
-                "vmousedown": function( event ) {
-                    popup.show();
-                    text.hide();
-                    sliderHandle.addClass("ui-slider-handle-pressed");
-
-                    var val = inputElement.val();
-                    popupText.html(val);
-                },
-                "vmousecancel vmouseup": function( event ) {
-                    popup.hide();
-                    sliderHandle.removeClass("ui-slider-handle-pressed");
-                    var val = inputElement.val();
-                    text.html(val);
-                    text.show();
-                }
-            });
-
-            // set initial value of handle text
-            var val = inputElement.val();
-            text.html(val);
-
-            // bind handle text to updates
-            inputElement.change( function(e) {
-                var val = $(e.target).val();
-                text.html(val);
-                popupText.html(val);
-            });
-
-            // hide input element
-            // not in nbeat but need it to get value
+            // hide the slider input element proper
             inputElement.hide();
-        },
-    }); /* End of widget */
 
-    // auto self-init widgets
+            // get the element where value can be displayed
+            var handleText = inputElement.next('.ui-slider').find('.ui-btn-text');
+
+            // show value in that element
+            var updateHandleText = function () {
+                handleText.html(self.element.val());
+            };
+
+            updateHandleText();
+
+            // bind to changes in the slider's value to update handle text
+            this.element.bind('change', updateHandleText);
+        },
+    });
+
     // stop jqm from initialising sliders
     $(document).bind("pagebeforecreate", function (e) {
-        if ( $.data( window, "realInitSelector" ) === undefined ) {
-            $.data( window,"realInitSelector", $.mobile.slider.prototype.options.initSelector );
-
+        if ($.data(window, "realInitSelector") === undefined ) {
+            $.data(window,"realInitSelector", $.mobile.slider.prototype.options.initSelector);
             $.mobile.slider.prototype.options.initSelector = null;
         }
     });
