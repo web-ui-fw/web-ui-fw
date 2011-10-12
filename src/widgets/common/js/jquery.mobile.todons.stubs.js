c9a9959355eeab5ae82e8bd4644b6c1ccddc807d
Salvatore Iovene <salvatore.iovene@intel.com>
2011-10-12 11:19:32 +0300

    stubs: removed unneeded functions.

--- src/widgets/common/js/jquery.mobile.todons.stubs.js
+++ src/widgets/common/js/jquery.mobile.todons.stubs.js
@@ -22,66 +22,6 @@ jQuery.extend(jQuery.mobile.todons, {
         }
     },
 
-    PersonArraySuccessCallback: function() {
-        this.onsuccess = function(personArray) {
-        }
-    },
-
-    ErrorCallback: function() {
-        this.onerror = function(error) {
-        }
-    },
-
-    Filter: function() {
-        var ID_FILTER = 0;
-        var UNION_FILTER = 1;
-        var INTERSECTION_FILTER = 2;
-        var ATTRIBUTE_FILTER = 3;
-        var ATTRIBUTE_RANGE_FILTER = 4;
-
-        var type = ID_FILTER;
-    },
-
-    SortOrderArray: function() {
-    },
-
-    DeviceAPIError: function() {
-        var code;
-        var message;
-
-        var INDEX_SIZE_ERR                 = 1;
-        var DOMSTRING_SIZE_ERR             = 2;
-        var HIERARCHY_REQUEST_ERR          = 3;
-        var WRONG_DOCUMENT_ERR             = 4;
-        var INVALID_CHARACTER_ERR          = 5;
-        var NO_DATA_ALLOWED_ERR            = 6;
-        var NO_MODIFICATION_ALLOWED_ERR    = 7;
-        var NOT_FOUND_ERR                  = 8;
-
-        var NOT_SUPPORTED_ERR              = 9;
-        var INUSE_ATTRIBUTE_ERR            = 10;
-        var INVALID_STATE_ERR              = 11;
-        var SYNTAX_ERR                     = 12;
-        var INVALID_MODIFICATION_ERR       = 13;
-        var NAMESPACE_ERR                  = 14;
-        var INVALID_ACCESS_ERR             = 15;
-        var VALIDATION_ERR                 = 16;
-
-        var TYPE_MISMATCH_ERR              = 17;
-      
-        var SECURITY_ERR                   = 18;
-
-        var NETWORK_ERR                    = 19;
-      
-        var ABORT_ERR                      = 20;
-
-        var TIMEOUT_ERR                    = 21;
-
-        var INVALID_VALUES_ERR             = 22;
-
-        var POLICY_ERR                     = 23;
-    },
-
     Person: function(id) {
         this._id = id;
 
