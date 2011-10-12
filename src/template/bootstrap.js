795374cc26944799b1103c924cb186a176557c4f
Elliot Smith <elliot.smith@intel.com>
2011-10-12 23:13:30 +0100

    [theming] Add 'tizen' to list of supported themes

--- src/template/bootstrap.js
+++ src/template/bootstrap.js
@@ -146,7 +146,7 @@ S = {
     defaultFrameworkTheme: 'default',
 
     frameworkVersions: ['0.1'],
-    frameworkThemes: ['default']
+    frameworkThemes: ['default', 'tizen']
 };
 
 /* Create custom user stylesheet */
