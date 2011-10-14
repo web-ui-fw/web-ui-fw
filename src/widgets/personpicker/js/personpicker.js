0eb12b6bdbd7e8e50c599cd5175551a0650cfc3e
Elliot Smith <elliot.smith@intel.com>
2011-10-14 16:40:29 +0100

    Revert "[personpicker] Use scrollistview (groupindex) on list"
    This reverts commit 6b03ddb6220320aec4ae4fde70156753c8473211.
    
    I pulled this because scrolllistview doesn't work nicely with
    shortcutscroll at the moment, but scrollview does; shortcutscroll
    is probably more useful to have than scrolllistview.

--- src/widgets/personpicker/js/personpicker.js
+++ src/widgets/personpicker/js/personpicker.js
@@ -95,8 +95,7 @@
                     });
             });
 
-            self._data.ui.container.scrolllistview({direction: 'y'});
-            self._data.ui.container.addClass('ui-scrolllistview');
+            self._data.ui.list.parent().scrollview({direction: 'y'});
             self._data.ui.search.textinput('enable');
         },
 
@@ -105,7 +104,6 @@
 
             self._data.ui = {
                 personpicker: ".ui-personpicker",
-                container: ".ui-personpicker-container",
                 list: ".ui-personpicker ul"
             };
 
