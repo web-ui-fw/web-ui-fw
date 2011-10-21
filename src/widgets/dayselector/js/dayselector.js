701817d6163e76ae1ebb1e2094d2c52dcc57ecc5
Elliot Smith <elliot.smith@intel.com>
2011-10-21 13:09:37 +0100

    [dayselector] Correctly apply theme

--- src/widgets/dayselector/js/dayselector.js
+++ src/widgets/dayselector/js/dayselector.js
@@ -12,10 +12,10 @@
  * containing buttons in a horizontal
  * the widget can be invoked like ->
  * e.g. $('#dayselector').dayselector()
- * 
+ *
  * $("#daySelector1").dayselector('value') returns the values of the selected checkboxes
- *  
- * selectAll() : method can be used to select all the options in the fieldset ( all days of the week) 
+ *
+ * selectAll() : method can be used to select all the options in the fieldset ( all days of the week)
  *
  * Options:
  * -> data-theme : override the data-theme of the widget
@@ -23,32 +23,31 @@
 (function ($, window, undefined) {
     $.widget("todons.dayselector", $.mobile.widget, {
         options: {
-            theme: 'e'
+            theme: 'c'
         },
-
 
         _create: function () {
             var self = this,
                 proto = $.mobile.todons.loadPrototype("dayselector"),
                 themeClass;
 
-            proto.insertBefore(this.element);
+            var topElement = proto.find('.daycheckboxes');
+
+            topElement.insertBefore(this.element);
 
             // theming; override default with the slider's theme if present
-            this.options.theme = this.element.data('theme') || this.options.theme;
-            themeClass = 'ui-body-' + this.options.theme;
-            var daySelectorTheme = proto.find('.ui-dayselector');
-            daySelectorTheme.attr('data-theme', this.options.theme);
-            daySelectorTheme.addClass(themeClass);
+            this.options.theme = this.element.data('theme') ||
+                                 this.element.closest(':jqmData(theme)').attr('data-theme') ||
+                                 this.options.theme;
 
-            this.checkboxes = proto.find('.custom-checkbox').checkboxradio();
-
+            this.checkboxes = topElement.find('.custom-checkbox').checkboxradio({theme: this.options.theme});
         },
 
         value: function () {
             var values = $('input:checkbox:checked.custom-checkbox').map(function () {
                 return this.value;
             }).get();
+
             return values.join(',');
         },
 
