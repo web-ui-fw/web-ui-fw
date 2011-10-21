59d7eafb7b732279163bc885d7d298f668bcb2dc
Elliot Smith <elliot.smith@intel.com>
2011-10-21 10:34:03 +0100

    [hbox] Add API docs

--- src/widgets/layout-hbox/js/layout-hbox.js
+++ src/widgets/layout-hbox/js/layout-hbox.js
@@ -8,6 +8,47 @@
 
 /**
  * Horizontal box layout extension.
+ *
+ * This will arrange the child elements of a container in a horizontal
+ * row. This only makes sense if your container is a div
+ * and contains children which are also divs; the children should
+ * also have a height and width set in CSS, otherwise the layout
+ * manager won't know what to do with them.
+ *
+ * Apply it by setting data-layout="hbox" on a container element
+ * or calling $(element).layouthbox().
+ *
+ * Usually, you would use a div as the container to get the right effect
+ * (an element with display:block).
+ *
+ * Options can be set programmatically:
+ *
+ *
+ *
+ * or via a data-layout-options attribute on the container:
+ *
+ *   <div data-layout="hbox" data-layout-options='{"hgap":5}'>
+ *       <div>child 1</div>
+ *       <div>child 2</div>
+ *   </div>
+ *
+ * If you change any options after creating the widget, call
+ * $(element).layouthbox('refresh') to have them picked up.
+ *
+ * Options:
+ *
+ *   {Integer} hgap (default=0)
+ *   Horizontal gap (in pixels) between the child elements.
+ *
+ *   {Boolean} scrollable (default=true)
+ *   Set to true to enable a scrollview on the
+ *   container. If false, children will be clipped if
+ *   they fall outside the edges of the container after
+ *   layouting.
+ *
+ *   {Boolean} showScrollBars (default=true)
+ *   Set to false to hide scrollbars on the container's scrollview.
+ *   Has no effect is scrollable=false
  */
 
 (function ($, undefined) {
