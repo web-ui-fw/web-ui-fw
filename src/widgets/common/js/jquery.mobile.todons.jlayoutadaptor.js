56791b2faa5f5818ebeb0efbee381b43dfbad806
Elliot Smith <elliot.smith@intel.com>
2011-10-24 11:58:49 +0100

    [layout-box] Refactor so vbox and hbox share a JS file

---
+++ src/widgets/common/js/jquery.mobile.todons.jlayoutadaptor.js
@@ -0,0 +1,98 @@
+/**
+ * Wrapper round the jLayout functions to enable it to be used
+ * for creating jQuery Mobile layout extensions.
+ *
+ * See the layouthbox and layoutvbox widgets for usage examples.
+ */
+(function ($, undefined) {
+
+$.widget("todons.jlayoutadaptor", $.mobile.widget, {
+    options: {
+        hgap: null,
+        vgap: null,
+        scrollable: true,
+        showScrollBars: true,
+        direction: null
+    },
+
+    _create: function () {
+        var self = this,
+            options = this.element.data('layout-options'),
+            page = $(this.element).closest(':jqmData(role="page")');
+
+        $.extend(this.options, options);
+
+        if (page && !page.is(':visible')) {
+            this.element.hide();
+
+            page.bind('pageshow', function () {
+                self.refresh();
+            });
+        }
+        else {
+            this.refresh();
+        }
+    },
+
+    refresh: function () {
+        var container;
+        var config = $.extend(this.options, this.fixed);
+
+        if (config.scrollable) {
+            if (!(this.element.children().is('.ui-scrollview-view'))) {
+                // create the scrollview
+                this.element.scrollview({direction: config.direction,
+                                         showScrollBars: config.showScrollBars});
+            }
+            else if (config.showScrollBars) {
+                this.element.find('.ui-scrollbar').show();
+            }
+            else {
+                this.element.find('.ui-scrollbar').hide();
+            }
+
+            container = this.element.find('.ui-scrollview-view');
+        }
+        else {
+            container = this.element;
+        }
+
+        container.layout(config);
+
+        this.element.show();
+
+        if (config.scrollable) {
+            // get the right/bottom edge of the last child after layout
+            var lastItem = container.children().last();
+
+            var edge;
+
+            if (config.direction === 'x') {
+                edge = lastItem.position().left +
+                       lastItem.outerWidth(true);
+
+                // manually reset the width of the div so it horizontally
+                // fills its parent
+                this.element.width('100%');
+
+                // set the scrollview's view width to the original width
+                this.element.find('.ui-scrollview-view')
+                            .width(edge);
+            }
+            else if (config.direction === 'y') {
+                edge = lastItem.position().top +
+                       lastItem.outerHeight(true);
+
+                // manually reset the height of the div so it vertically
+                // fills its parent
+                this.element.height('100%');
+
+                // set the scrollview's view height to the original height
+                this.element.find('.ui-scrollview-view')
+                            .height(edge);
+            }
+        }
+    }
+});
+
+})(jQuery);
