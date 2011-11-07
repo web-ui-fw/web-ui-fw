189542d9e7917a971236b88a1015abf1d0ae2e7a
Elliot Smith <elliot.smith@intel.com>
2011-11-07 16:45:09 +0000

    [listviewcontrols] Add basic init and option parsing

--- src/widgets/listviewcontrols/js/listviewcontrols.js
+++ src/widgets/listviewcontrols/js/listviewcontrols.js
@@ -24,7 +24,8 @@
 // To associate the listview with the control panel, add
 // data-listviewcontrols="..selector.." to a listview, where
 // selector selects a single form element (the control panel
-// you defined). If you then call listviewcontrols('mode', 'edit') on the
+// you defined). If you then call
+// listviewcontrols('option', 'mode', 'edit') on the
 // listview, the controls are made visible (this is just a proxy for
 // calling show() on the associated form containing the controls).
 //
@@ -44,17 +45,119 @@
 // up to you to define the behaviour associated with interactions on
 // the control panel and/or controls inside list items.
 //
-// Options:
+// Options (set in options hash passed to constructor, or via the
+// option method, or declaratively by attribute described below):
+//
+//   controlsSelector {String}
+//     Selector string for selecting the form element representing the
+//     control panel for the listview. The context for find() is the
+//     document (to give the most flexibility), so your selector
+//     should be specific. Set declaratively with
+//       data-listviewcontrols="...selector...".
 //
 //   mode {String='edit'||'view'}
-//   Mode for the controls
+//     Mode for the controls.
+//     Set declaratively with
+//       data-listviewcontrols-options='{"mode":"<mode>"}'.
 
 (function ($) {
 
-var listviewcontrols = function (options) {
-};
+$.widget("todons.listviewcontrols", $.mobile.widget, {
+    options: {
+        mode: 'view',
+        controlsSelector: null
+    },
 
-$.fn.listviewcontrols = listviewcontrols;
+    _create: function (options) {
+        options = options || {};
+
+        var self = this,
+            optionsValid = true,
+            listview = $(this.element),
+            page = listview.closest('.ui-page'),
+            controlsSelectorAttr,
+            controlsSelector,
+            dataOptions;
+
+        controlsSelectorAttr = 'data-' + $.mobile.ns + 'listviewcontrols';
+        controlsSelector = listview.attr(controlsSelectorAttr);
+        options['controlsSelector'] = controlsSelector;
+
+        dataOptions = listview.jqmData('listviewcontrols-options');
+
+        // precedence for options: defaults < jqmData attribute < options arg
+        $.extend(dataOptions, this._defaults);
+        $.extend(options, dataOptions);
+
+        optionsValid = optionsValid &&
+                       this._validOption('controlsSelector', options.controlsSelector) &&
+                       this._validOption('mode', options.mode);
+
+        if (!optionsValid) {
+            console.error('Could not create listviewcontrols widget due to ' +
+                          'invalid option(s)');
+            return;
+        }
+
+        this.options = options;
+
+        this.controls = $(document).find(options.controlsSelector).first();
+
+        if (this.controls.length === 0) {
+            console.error('Could not create listviewcontrols widget: ' +
+                          'controlsSelector didn\'t select any elements');
+            return;
+        }
+
+        if (page && !page.is(':visible')) {
+            page.bind('pageshow', function () {
+                self.refresh();
+            });
+        }
+        else {
+            this.refresh();
+        }
+    },
+
+    destroy: function () {
+    },
+
+    refresh: function () {
+    },
+
+    _validOption: function (name, value) {
+        switch (name) {
+            case 'mode':
+                if (!(value == 'view' || value == 'edit')) {
+                    console.error('Invalid mode for listviewcontrols widget ' +
+                                  '(should be "view" or "edit")');
+                    return false;
+                }
+            case 'controlsSelector':
+                if (!value) {
+                    console.error('Invalid controlsSelector for ' +
+                                  'listviewcontrols widget');
+                    return false;
+                }
+        };
+
+        return true;
+    },
+
+    _setOption: function (name, value) {
+        var oldValue, newValue;
+
+        oldValue = this.options[name];
+
+        if (this._validOption(name, value)) {
+            newValue = this.options[name];
+
+            if (oldValue != newValue) {
+                this.refresh();
+            }
+        }
+    }
+});
 
 $('ul').live('listviewcreate', function () {
 	var list = $(this);
