931ac7adc0fb839843b3be2dfa6665506f685173
Elliot Smith <elliot.smith@intel.com>
2011-11-24 14:53:12 +0000

    [optionheader] Remove click handlers before adding them
    Ensure that click handler isn't bound multiple times.

--- src/widgets/optionheader/js/optionheader.js
+++ src/widgets/optionheader/js/optionheader.js
@@ -3,20 +3,20 @@
  *
  * This software is licensed under the MIT licence (as defined by the OSI at
  * http://www.opensource.org/licenses/mit-license.php)
- * 
+ *
  * ***************************************************************************
  * Copyright (C) 2011 by Intel Corporation Ltd.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation
  * the rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -211,8 +211,8 @@ $.widget("todons.optionheader", $.todons.widgetex, {
 
         // if expandable, bind clicks to the toggle() method
         if (this.options.expandable) {
-            el.bind('vclick', this.clickHandler);
-            arrow.bind('vclick', this.clickHandler);
+            el.unbind('vclick', this.clickHandler).bind('vclick', this.clickHandler);
+            arrow.unbind('vclick', this.clickHandler).bind('vclick', this.clickHandler);
         }
         else {
             el.unbind('vclick', this.clickHandler);
@@ -268,7 +268,7 @@ $.widget("todons.optionheader", $.todons.widgetex, {
         // combine commonCallback with any user-specified callback
         if (options.callback) {
             callback = function () {
-                options.callback();
+                options.callback();.bind('vclick', this.clickHandler)
                 commonCallback();
             };
         }
