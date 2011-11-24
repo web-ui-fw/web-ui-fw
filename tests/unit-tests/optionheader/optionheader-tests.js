d5adad33629d90f155c0b4b29134f7b71e2e5fa7
Elliot Smith <elliot.smith@intel.com>
2011-11-24 16:42:13 +0000

    [optionheader] Test basic option settings

--- tests/unit-tests/optionheader/optionheader-tests.js
+++ tests/unit-tests/optionheader/optionheader-tests.js
@@ -5,21 +5,41 @@
 (function ($) {
   $.mobile.defaultTransition = "none";
 
-	module("Option header");
+  module("Option header");
 
-	asyncTest("description here", function () {
-		$.testHelper.pageSequence([
-			function () {
-				$.testHelper.openPage('#optionheader-test');
-			},
+  asyncTest("Should set options from data-options", function () {
+    $.testHelper.pageSequence([
+      function () {
+        $.testHelper.openPage('#optionheader-test-options');
+      },
 
-			function() {
-				var $new_page = $('#optionheader-test');
+      function() {
+        var $new_page = $('#optionheader-test-options');
 
-				ok($new_page.hasClass('ui-page-active'));
+        ok($new_page.hasClass('ui-page-active'));
 
-				start();
-			}
-		]);
-	});
+        // test default options set
+        var defaultOh = $($new_page.find('#optionheader-test-options-default'));
+        equal(defaultOh.optionheader('option', 'showIndicator'), true);
+        equal(defaultOh.optionheader('option', 'theme'), 'b');
+        equal(defaultOh.optionheader('option', 'startCollapsed'), false);
+        equal(defaultOh.optionheader('option', 'expandable'), true);
+        equal(defaultOh.optionheader('option', 'duration'), 0.25);
+
+        // test custom options set
+        var customOh  = $($new_page.find('#optionheader-test-options-custom'));
+        equal(customOh.optionheader('option', 'showIndicator'), false);
+        equal(customOh.optionheader('option', 'theme'), 'a');
+        equal(customOh.optionheader('option', 'startCollapsed'), true);
+        equal(customOh.optionheader('option', 'expandable'), false);
+        equal(customOh.optionheader('option', 'duration'), 0.5);
+
+        // test data-theme attribute - should override data-options
+        var themedOh = $($new_page.find('#optionheader-test-theme-attr'));
+        equal(themedOh.optionheader('option', 'theme'), 'c');
+
+        start();
+      }
+    ]);
+  });
 })(jQuery);
