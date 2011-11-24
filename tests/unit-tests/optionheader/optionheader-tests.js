d7210bd40ea064210d9f6f98f80ada4dcf1ed374
Elliot Smith <elliot.smith@intel.com>
2011-11-24 17:39:35 +0000

    [optionheader] Add basic markup tests

--- tests/unit-tests/optionheader/optionheader-tests.js
+++ tests/unit-tests/optionheader/optionheader-tests.js
@@ -42,4 +42,35 @@
       }
     ]);
   });
+
+  asyncTest("Should create correct markup", function () {
+    $.testHelper.pageSequence([
+      function () {
+        $.testHelper.openPage('#optionheader-test-markup');
+      },
+
+      function() {
+        var $new_page = $('#optionheader-test-markup');
+
+        ok($new_page.hasClass('ui-page-active'));
+
+        // test indicator markup
+        var withIndicator = $($new_page.find('#optionheader-test-markup-indicator'));
+        equal(withIndicator.siblings('.ui-option-header-triangle-arrow').length, 1);
+        var withoutIndicator = $($new_page.find('#optionheader-test-markup-no-indicator'));
+        equal(withoutIndicator.siblings('.ui-option-header-triangle-arrow').length, 0);
+
+        // test for classes
+        ok(withIndicator.hasClass('ui-option-header'), 'should have base class');
+        ok(withIndicator.hasClass('ui-option-header-1-row'), 'should default to 1 row');
+        ok(withIndicator.hasClass('ui-body-b'), 'should have class for default b theme');
+
+        // test for custom theme class
+        var themeSet = $($new_page.find('#optionheader-test-markup-custom-theme'));
+        ok(themeSet.hasClass('ui-body-c'), 'should have class for theme c set in options');
+
+        start();
+      }
+    ]);
+  });
 })(jQuery);
