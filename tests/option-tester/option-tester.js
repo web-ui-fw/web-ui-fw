592b918af61729f4be8c54e8b001ac123a3ff2bf
Gabriel Schulhof <gabriel.schulhof@intel.com>
2011-12-12 15:42:12 +0200

    [option-tester] Display inputs for each option

--- tests/option-tester/option-tester.js
+++ tests/option-tester/option-tester.js
@@ -1,17 +1,82 @@
 (function($, undefined) {
     var init = false;
 
+    function formatHTML(str) {
+        var ret = "", nIndent = -4, startIdx = 0, snip = "";
+        for (var idx = str.indexOf(">", 0); idx != -1 ; idx = str.indexOf(">", ++idx)) {
+            snip = (str.substring(startIdx, idx) + ">" + "\n").replace(/^[ \t]*/, "");
+            if (snip.substring(0, 2) === "</")
+                nIndent -= 4;
+            else
+            if (snip.substring(0, 1) === "<")
+                nIndent += 4;
+            for (var Nix = 0 ; Nix < nIndent ; Nix++)
+                ret = ret + " ";
+            ret = ret + snip;
+            startIdx = idx + 1;
+        }
+
+        return ret;
+    }
+
+    function createOption(widgetType, theWidget, key) {
+        var optionsList = $("#options-list"), entry;
+
+        function makeSetter() {
+            switch(typeof $.todons[widgetType].prototype.options[key]) {
+                case "boolean":
+                    console.log("theWidget[" + widgetType + "]('option', " + key + "): ");
+                    console.log(theWidget[widgetType]("option", key));
+                    return {
+                        html: $("<input/>", {type: "checkbox"}),
+                        widget: {
+                            type: "toggleswitch",
+                            options: {
+                                checked: theWidget[widgetType]("option", key)
+                            }
+                        }
+                    };
+
+                case "integer":
+                    return {
+                        html: $("<input/>", {type: "number"})
+                    };
+
+                default:
+                    return { 
+                        html: $("<input/>", {type: "text", value: typeof $.todons[widgetType].prototype.options[key], id: key + "-option"}),
+                    };
+            }
+        }
+
+        entry = makeSetter();
+        $("<div/>").fieldcontain()
+            .append($("<label/>", {"for": key + "-option"}).text(key))
+            .append(entry.html)
+            .appendTo(optionsList);
+        if (entry.widget !== undefined)
+            entry.html[entry.widget.type](entry.widget.options);
+    }
+
     function createWidget(widgetType, inputType) {
-        console.log("createWidget(" + widgetType + ", " + inputType + ")");
-        $("#widget-container").empty();
+        var theWidget;
+        $("#widget-container").empty().css("display", "none");
         if (inputType !== null) {
             $("#widget-container").html(
                 "<form action='#' method='get'>" +
                 "   <input type='" + inputType + "' name='testInput' id='testInput'></input>" +
                 "</form>"
             );
-            $("#testInput")[widgetType]();
+            theWidget = $("#testInput")[widgetType]();
         }
+        else
+            theWidget = $("<div></div>").appendTo("#widget-container")[widgetType]();
+        $("#widget-container").removeAttr("style").css("border", "1px dashed black");
+        $("#widget-src").html($("<pre><code>").text(formatHTML($("#widget-container").html())));
+        $("#options-list");
+        $.each($.todons[widgetType].prototype.options, function(key) {
+            createOption(widgetType, theWidget, key);
+        });
     }
 
     $(document).bind("pageshow", function() {
