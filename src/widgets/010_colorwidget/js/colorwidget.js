80f3641ed875de698528a44195003e575b31eb84
Gabriel Schulhof <gabriel.schulhof@intel.com>
2011-12-20 18:37:29 +0200

    [colorwidget] Add HSLToGray for computing grayscale from HSL

--- src/widgets/010_colorwidget/js/colorwidget.js
+++ src/widgets/010_colorwidget/js/colorwidget.js
@@ -264,6 +264,48 @@ $.todons.colorwidget.clrlib = {
     // l is in [0,   1]
     RGBToHSL: function(rgb) {
         return $.todons.colorwidget.clrlib.HSVToHSL($.todons.colorwidget.clrlib.RGBToHSV(rgb));
+    },
+
+    // Converts hsl to grayscale
+    // Full-saturation magic grayscale values were taken from the Gimp
+    //
+    // Input: [ h, s, l ], where
+    // h is in [0, 360]
+    // s is in [0,   1]
+    // l is in [0,   1]
+    //
+    // Returns: [ r, g, b ], where
+    // r is in [0,   1]
+    // g is in [0,   1]
+    // b is in [0,   1]
+    HSLToGray: function(hsl) {
+        var intrinsic_vals = [0.211764706, 0.929411765, 0.71372549, 0.788235294, 0.070588235, 0.28627451, 0.211764706],
+            idx = Math.floor(hsl[0] / 60),
+            begVal, endVal, val;
+
+        // Find hue interval
+        begVal = intrinsic_vals[idx];
+        endVal = intrinsic_vals[idx + 1];
+
+        // Adjust for lum
+        if (hsl[2] < 0.5) {
+            var lowerHalfPercent = hsl[2] * 2;
+            begVal *= lowerHalfPercent;
+            endVal *= lowerHalfPercent;
+        }
+        else {
+            var upperHalfPercent = (hsl[2] - 0.5) * 2;
+            begVal += (1.0 - begVal) * upperHalfPercent;
+            endVal += (1.0 - endVal) * upperHalfPercent;
+        }
+
+        // This is the gray value at full sat, whereas hsl[2] is the gray value at 0 sat.
+        val = begVal + ((endVal - begVal) * (hsl[0] - (idx * 60))) / 60;
+
+        // Get value at hsl[1]
+        val = val + (hsl[2] - val) * (1.0 - hsl[1]);
+
+        return [val, val, val];
     }
 };
 
