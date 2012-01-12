3cbed1c575497889959bc52f82fcfb28a9f275ad
Gabriel Schulhof <gabriel.schulhof@intel.com>
2012-01-12 16:14:49 +0200

    [inspect-page] Adding

---
+++ tests/inspect-page/inspect-page.js
@@ -0,0 +1,147 @@
+/*
+ * jQuery Mobile Widget @VERSION
+ *
+ * This software is licensed under the MIT licence (as defined by the OSI at
+ * http://www.opensource.org/licenses/mit-license.php)
+ *
+ * ***************************************************************************
+ * Copyright (C) 2011 by Intel Corporation Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ * ***************************************************************************
+ *
+ * Authors: Gabriel Schulhof <gabriel.schulhof@intel.com>
+ */
+
+// inspect-page
+// Inspect and set options for widgets on a jQuery Mobile page
+//
+// To use:
+// 1. Add inspect-page.js, option-list.js, and inspect-page.html to the folder where the app under test is located
+// 2. Add a reference to this script (inspect-page.js), and to option-list.js to the list of scripts loaded from the app
+// under test
+//
+// The result:
+// Every page of the app which has a jQuery Mobile header (<div data-role='header'>) will have a button on the right
+// labelled "Test Options". When you click this button, a list of all widgets found on the current page will be
+// displayed. If more than one widget is associated with a given element, each one will be shown. When you click on a
+// widget, its options will be shown editably on the right of the list. Modifications to the options will result in a
+// <element>.widgetname("option", <optionName>, <newValue>) call to the widget.
+//
+// To customize:
+// After including this file in your app, you can overwrite the "addInspectionTrigger" function to do something other
+// than what the default does (which is to add the button to the page header), and, within the new handler, you can call
+// "launchInspector" from the context of an element of your choosing. The context is only important if you also have
+// web-ui-fw defined in your app, because the coordinates of the context element are used for launching a popup window
+// which contains the widget list and the option list
+
+(function($, undefined) {
+
+    var havePopupwindow = ($.todons && $.todons.popupwindow);
+
+    function elemName(elem) {
+        var str = elem.tagName
+            + ($(elem).attr("id") === undefined ? "" : ("#" + $(elem).attr("id")))
+            + ($(elem).attr("class") === undefined ? ""
+                : ("." + $(elem).attr("class").replace(" ", ".")));
+
+        if (str.length > 40)
+            str = str.substring(0, 40) + "...";
+
+        return str;
+    }
+
+    function launchInspector() {
+        var src = $(this),
+            page = src.closest(":jqmData(role='page')"),
+            widgetList = $("#widget-list").empty(),
+            optionList = $("#option-list"),
+            realList = $("<ul data-role='listview' data-scroll='y' class='inspect-page-pane'></ul>")
+                .appendTo(widgetList);
+
+        if (optionList.data("optionlist"))
+            optionList.optionlist("destroy");
+
+        page.find("*").each(function() {
+            var widgets = $.todons.optionlist.widgetsFromElement(this);
+            if (widgets) {
+                realList.append("<li data-role='list-divider'>" + elemName(this) + "</li>");
+                $.each(widgets, function(key, value) {
+                    $("<li><a>" + value.namespace + "." + value.widgetName + "</a></li>")
+                        .appendTo(realList)
+                        .find("a")
+                        .bind("vclick", function() {
+                            if (optionList.data("optionlist"))
+                                optionList.optionlist("destroy");
+                            optionList.optionlist();
+                            optionList.optionlist("option", "widget", value);
+                        });
+                });
+            }
+        });
+
+        if (!havePopupwindow) {
+            listPage = realList.closest(":jqmData(role='page')");
+            if (listPage.data("page")) {
+                realList.listview();
+                if ($.mobile.scrollview)
+                    realList.scrollview();
+            }
+        }
+        else
+            realList.trigger("create");
+
+        if (havePopupwindow) {
+            var changePage = true,
+                content = widgetList.closest(":jqmData(role='content')");
+
+            if (content.length > 0) {
+                content
+                    .appendTo(page)
+                    .trigger("create")
+                    .removeAttr("style")
+                    .popupwindow({fade: false})
+                    .popupwindow("option", "overlayTheme", src.jqmData("theme"))
+                    .bind("closed", function() {
+                        $(this).popupwindow("destroy").css("display", "none");
+                    })
+                    .popupwindow("open",
+                        src.offset().left + src.width() / 2,
+                        src.offset().top + src.height() / 2);
+                changePage = false;
+            }
+
+            if (changePage)
+                $.mobile.changePage("#inspect-page");
+        }
+    }
+
+    var addInspectionTrigger = function(elem) {
+        var btn = $("<a " + (havePopupwindow ? "" : "href='#inspect-page'") + " data-remove-ghost-button='true' class='ui-btn-right' data-iconpos='left' data-icon='grid'>Test Options</a>")
+            .appendTo($(":jqmData(role='header')"))
+            .buttonMarkup()
+            .bind("vclick", launchInspector);
+    }
+
+    $(document)
+        .ready(function() {
+            $.mobile.loadPage("inspect-page.html");
+        })
+        .bind("pagecreate", function(e) { addInspectionTrigger(e.target); });
+})(jQuery);
