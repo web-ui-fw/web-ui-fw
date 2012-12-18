04cd1beeaf39e302e221d6c71503b17b9a8a7ed2
Gabriel Schulhof <gabriel.schulhof@intel.com>
2012-12-18 22:43:12 +0200

    Tabula rasa

--- demos/gallery/before-jqm.js
+++
@@ -1,8 +0,0 @@
-// This code runs before we unleash jQuery Mobile onto the document
-
-$(document).bind("mobileinit", function() {
-    // Prevent jqm from turning #input-switch into a nice-looking checkbox.
-    // After all, we want to turn it into a nice-looking switch.
-    $.mobile.checkboxradio.prototype.options.initSelector =
-        ":not(:not(" + $.mobile.checkboxradio.prototype.options.initSelector + "))[id!='input-switch']";
-});
