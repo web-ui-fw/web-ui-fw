7eb354135cde545cd4e3c1659881d577bcfb5778
Gabriel Schulhof <gabriel.schulhof@intel.com>
2012-12-19 22:47:54 +0200

    Establishing basic infrastructure

---
+++ demos/_assets/js/index.php
@@ -0,0 +1,8 @@
+<?php
+$type = 'text/javascript';
+$files = array(
+	'web-ui-fw-demos.js',
+	'../../../jqm/docs/demos/_assets/js/view-source.js'
+);
+
+require_once( '../../../jqm/combine.php' );
---
+++ js/index.php
@@ -0,0 +1,54 @@
+<?php
+$type = 'text/javascript';
+$files = array(
+);
+
+function getGitHeadPath() {
+	$gitRoot = "../";
+	$gitDir = ".git";
+	$path = $gitRoot . $gitDir;
+
+	if ( is_file( $path ) && is_readable( $path ) ) {
+		$contents = file_get_contents( $path );
+		if ( $contents ) {
+			$contents = explode( " ", $contents );
+			if ( count( $contents ) > 1 ) {
+				$contents = explode( "\n", $contents[ 1 ] );
+				if ( $contents && count( $contents ) > 0 ) {
+					$path = $gitRoot . $contents[ 0 ];
+				}
+			}
+		}
+	}
+
+	return $path . "/logs/HEAD";
+}
+
+function getCommitId() {
+	$gitHeadPath = getGitHeadPath();
+
+	if ( $gitHeadPath ) {
+		$logs = ( is_readable( $gitHeadPath ) ? file_get_contents( $gitHeadPath ) : false );
+		if ( $logs ) {
+			$logs = explode( "\n", $logs );
+			$n_logs = count( $logs );
+			if ( $n_logs > 1 ) {
+				$log = explode( " ", $logs[ $n_logs - 2 ] );
+				if ( count( $log ) > 1 ) {
+					return $log[ 1 ];
+				}
+			}
+		}
+	}
+
+	return false;
+}
+
+$comment = getCommitId();
+if ( !$comment ) {
+	unset( $comment );
+} else {
+	$comment = "/* git commitid " . $comment . " */\n";
+}
+
+require_once('../jqm/combine.php');
