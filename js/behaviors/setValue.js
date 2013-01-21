bcb100642f1843670a4f944f7f7f295d7a898e7e
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-01-21 11:30:48 +0200

    setValue: Set the option corresponding to the value during _create()

--- js/behaviors/setValue.js
+++ js/behaviors/setValue.js
@@ -8,6 +8,26 @@ define( [ "jquery", "../../jqm/js/jquery.mobile.core" ], function( $ ) {
 ( function( $, undefined ) {
 
 $.mobile.behaviors.setValue = {
+	// The widget factory automagically merges data-<option> attributes with the
+	// widget instance's options, but it does not merge the input's value with the
+	// widget's corresponding option if this widget is based on an input
+	_create: function() {
+		var inputType;
+		if ( this._value !== undefined && this.element.is( "input" ) ) {
+			inputType = this.element.attr( "type" );
+
+			// Special handling for checkboxes and radio buttons, where the presence
+			// of the "checked" attribute is really the value
+			if ( inputType === "checkbox" || inputType === "radio" ) {
+				this.options[ this._value.option ] = this.element[ 0 ].hasAttribute( "checked" );
+			} else {
+				this.options[ this._value.option ] = this.element.attr( "value" );
+			}
+		}
+
+		this._super();
+	},
+
 	_setValue: function( newValue ) {
 		var valueString, valueStringIsSet, inputType;
 
@@ -15,10 +35,10 @@ $.mobile.behaviors.setValue = {
 			valueString = this._value.makeString ? this._value.makeString( newValue ) : newValue;
 			valueStringIsSet = true;
 
-			this.element.attr( this._value.attr, valueString );
+			this.element.attr( "data-" + $.mobile.ns + this._value.option, valueString );
 
 			if ( this._value.signal !== undefined ) {
-				this.element.triggerHandler( this._value.signal, newValue );
+				this.element.trigger( this._value.signal, newValue );
 			}
 		}
 
