e72ca0c0f5c603b0998b8ff838d9fb20e6ae0a44
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-02-14 09:04:39 +0200

    Unit tests: Colorpalette: Ensure that _makePalette() creates a palette that contains the colour on which it is based

--- tests/unit/color/colorpalette/colorpalette_core.js
+++ tests/unit/color/colorpalette/colorpalette_core.js
@@ -2,8 +2,19 @@
 
 	module( "colorpalette" );
 
-	test( "Easy to pass", function() {
-		deepEqual( 0, 0, "Real easy" );
+	test( "_makePalette contains colour around which it's supposed to build a palette", function() {
+		var clr = "#23fe9a",
+			pal = $.mobile.colorpalette.prototype._makePalette( $.Color( clr ), 13 ),
+			found = false;
+
+		$.each( pal.split( "," ), function( key, value ) {
+			if ( clr === value ) {
+				found = true;
+				return false;
+			}
+		});
+
+		deepEqual( found, true, "Colour " + clr + " is present in the generated palette" );
 	});
 
 })( jQuery );
