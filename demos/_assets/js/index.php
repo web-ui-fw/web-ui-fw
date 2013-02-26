53ff6522eb9d5a099f5f14dedc856d529a75970b
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-02-26 14:12:16 +0200

    Demos: Fix path to view-source.js

--- demos/_assets/js/index.php
+++ demos/_assets/js/index.php
@@ -2,7 +2,7 @@
 $type = 'text/javascript';
 $files = array(
 	'web-ui-fw-demos.js',
-	'../../../jqm/docs/demos/_assets/js/view-source.js'
+	'../../../jqm/docs/_assets/js/view-source.js'
 );
 
 require_once( '../../../jqm/combine.php' );
