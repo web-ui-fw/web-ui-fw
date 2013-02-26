baf104b06397e1bfcbfdf8907732d3bed6b146e2
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-02-26 14:12:38 +0200

    Demos: Sync hsvpicker colours

--- demos/_assets/js/web-ui-fw-demos.js
+++ demos/_assets/js/web-ui-fw-demos.js
@@ -1,3 +1,4 @@
 $( document ).bind( "colorchanged", function( e, newValue ) {
 	$( ":mobile-colorpalette" ).colorpalette( "option", "color", newValue );
+	$( ":mobile-hsvpicker" ).hsvpicker( "option", "color", newValue );
 });
