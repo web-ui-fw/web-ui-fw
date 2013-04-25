1fc4dbdb17f2ab2708f81aa797dbf595f27a84fa
Gabriel Schulhof <gabriel.schulhof@intel.com>
2013-04-25 10:28:51 +0300

    js/web-ui-fw.js: Forgot to add the file.

---
+++ js/web-ui-fw.js
@@ -0,0 +1,19 @@
+//>>excludeStart("jqmBuildExclude", pragmas.jqmBuildExclude);
+//>>group: exclude
+
+define([
+	'require',
+	'widgets/color/grayscale',
+	'widgets/color/hsvpicker',
+	'widgets/color/colorpalette',
+	'widgets/color/colorwidget',
+	'widgets/virtualgrid',
+	'widgets/tokentextarea',
+	'behaviors/setValue',
+	'behaviors/optionDemultiplexer',
+	'behaviors/createOuter',
+	'web-ui-fw.reduceScope'
+], function( require ) {
+	require( [ 'jqm/jquery.mobile.init' ], function() {} );
+});
+//>>excludeEnd("jqmBuildExclude");
