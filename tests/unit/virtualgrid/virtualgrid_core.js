943ac16697c4d9c5ae3e5639c157350361f551ba
allsteady <allsteady@gmail.com>
2013-06-18 20:49:05 +0900

    Virtualgrid: add unit test.

--- tests/unit/virtualgrid/virtualgrid_core.js
+++ tests/unit/virtualgrid/virtualgrid_core.js
@@ -2,10 +2,21 @@
 
 	module( "virtualgrid" );
 
-	test( "_makePalette contains colour around which it's supposed to build a palette", function() {
-		var $virtualgrid = $(".ui-virtualgrid-view");
+	test( "Virtualgrid test cases.", function() {
+		var	$virtualgrid = $(".ui-virtualgrid-view"),
+			$view, toXPos = 0, toYPos = 220;
 
 		deepEqual( $virtualgrid.length  , 1, "Virtualgrid is created." );
+		$view = $virtualgrid.find( ".ui-virtualgrid-overthrow" ) [ 0 ];
+		$virtualgrid.virtualgrid( "scrollTo", toXPos, toYPos );
+
+		asyncTest ( "asynchronous test :  scroll move test.", function (){
+			setTimeout( function () {
+				deepEqual( $view.scrollLeft  , toXPos, "new x position." );
+				deepEqual( $view.scrollTop  , toYPos, "new y position" );
+				start();
+			}, 300 );
+		});
 	});
 
 })( jQuery );
