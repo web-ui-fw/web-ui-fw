cb682d0ef36b0e15599086cbe0e0f9103170cf0d
Gabriel Schulhof <gabriel.schulhof@intel.com>
2014-08-05 12:38:12 +0300

    Tokentextarea2: Completing grouping tests

--- tests/integration/tokentextarea2/grouping_core.js
+++ tests/integration/tokentextarea2/grouping_core.js
@@ -20,9 +20,14 @@ test( "Summary updates even when it is hidden", function() {
 
 	deepEqual( input.siblings( ".ui-tokentextarea2-summary" ).text(),
 		"+ 1", "Updating the summary works" );
+
+	input.tokentextarea2( "remove" );
+
+	deepEqual( input.siblings( ".ui-tokentextarea2-summary" ).text(), "",
+		"Calling remove() updates the summary" );
 });
 
-test( "Calling focusOut() activates summary display", function() {
+test( "Calling focusOut() activates summary display and focusIn() puts it back", function() {
 	var input = $( "#focus-out-test" );
 
 	deepEqual( input.parent().hasClass( "ui-tokentextarea2-grouped" ), false,
@@ -38,4 +43,21 @@ test( "Calling focusOut() activates summary display", function() {
 
 	deepEqual( input.siblings( ".ui-tokentextarea2-summary" ).is( ":visible" ), true,
 		"Summary visible after focusOut()" );
+
+	input.tokentextarea2( "focusIn" );
+
+	deepEqual( input.parent().hasClass( "ui-tokentextarea2-grouped" ), false,
+		"Class ui-tokentextarea2-grouped absent after focusIn()" );
+
+	deepEqual( input.siblings( ".ui-tokentextarea2-summary" ).is( ":visible" ), false,
+		"Summary hidden after focusOut()" );
+});
+
+test( "Pre-rendered", function() {
+	var input = $( "#pre-rendered-test" );
+
+	input.tokentextarea2( "add", "ryan@example.com" );
+
+	deepEqual( input.siblings( ".ui-tokentextarea2-summary" ).text(), "+ 2",
+		"Pre-rendered summary reflects changes immediately" );
 });
